80f9025595995a0c0d91296cd4a0b86667512eac
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-10-07 13:27:54 +0200

    Updates

---
+++ precache-manifest.8979d06566523497539a278d0a12e71e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c9b5752a20c3484a196fcd198716bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0a4a2d0170e91a7506",
+    "url": "/static/css/main.2271c3ff.chunk.css"
+  },
+  {
+    "revision": "a0e38cb21b4ee9bcad7f",
+    "url": "/static/js/2.7d184afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d184afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0a4a2d0170e91a7506",
+    "url": "/static/js/main.ea749c62.chunk.js"
+  },
+  {
+    "revision": "9a395c2dc5bc5f2a7953",
+    "url": "/static/js/runtime-main.555541c8.js"
+  },
+  {
+    "revision": "8fd9cc6ac915a62d3b5ebaedcc764ae9",
+    "url": "/static/media/join_us_resize.8fd9cc6a.jpg"
+  },
+  {
+    "revision": "1c87472137548ed6f238d78d36c7ebdf",
+    "url": "/static/media/welcome.1c874721.png"
+  }
+]);
